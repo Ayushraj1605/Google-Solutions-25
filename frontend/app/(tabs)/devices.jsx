b8591e614f41a7fd9e4a40c44be00c0f1a5b89a4
Suyash Mishra <suyashmishra145@gmail.com>
2025-02-02 22:54:35 +0530

    commit

--- frontend/app/(tabs)/devices.jsx
+++ frontend/app/(tabs)/devices.jsx
@@ -1,14 +1,49 @@
-import { View, Text, ScrollView } from 'react-native'
-import React from 'react';
-import '../../global.css';
-import Cards from '../../components/devicecards';
-import SearchBar from '../../components/searchbar';
+import React from 'react'
+import '../../global.css'
+import Cards from '../../components/devicecards'
+import SearchBar from '../../components/searchbar'
+// import AddDeviceButton from '../../components/adddevices'
+import {
+  View,
+  StyleProp,
+  ViewStyle,
+  Animated,
+  StyleSheet,
+  Platform,
+  ScrollView,
+  Text,
+  SafeAreaView,
+  I18nManager,
+} from 'react-native';
+import { AnimatedFAB } from 'react-native-paper';
 
-const devices = () => {
+const devices = ({
+  animatedValue,
+  visible,
+  extended,
+  label,
+  animateFrom,
+  style,
+  iconMode,
+}) => {
+
+  const [isExtended, setIsExtended] = React.useState(true);
+
+  const isIOS = Platform.OS === 'ios';
+
+  const onScroll = ({ nativeEvent }) => {
+    const currentScrollPosition =
+      Math.floor(nativeEvent?.contentOffset?.y) ?? 0;
+
+    setIsExtended(currentScrollPosition <= 0);
+  };
+
+  const fabStyle = { [animateFrom]: 16 };
+
   return (
     <View className="w-full h-full items-center bg-white">
       <SearchBar />
-      <ScrollView contentContainerStyle={{ alignItems: 'center' }} className="flex w-full h-full">
+      <ScrollView onScroll={onScroll} contentContainerStyle={{ alignItems: 'center' }} className="flex w-full h-full">
         <Cards />
         <Cards />
         <Cards />
@@ -22,10 +57,31 @@ const devices = () => {
         <Cards />
         <Cards />
       </ScrollView>
-      
+      <AnimatedFAB
+        icon={'plus'}
+        label={'  Add Device'}
+        extended={isExtended}
+        onPress={() => console.log('Pressed')}
+        visible={visible}
+        animateFrom={'left'}
+        iconMode={'static'}
+        color="white"
+        uppercaseLabel={false}
+        labelStyle={{ color: '#FFFFFF' }}
+        style={[styles.fabStyle, style, fabStyle]}
+      />
     </View>
   )
 }
 
 export default devices
 
+const styles = StyleSheet.create({
+  fabStyle: {
+    bottom: 16,
+    left: 16,
+    position: 'absolute',
+    backgroundColor: "#609966",
+    shadowColor: "#000",
+  },
+});
